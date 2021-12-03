dd122bcc47a770015e602cdb824d91b41739fd5c
mental_disaster <ghgh9244@gmail.com>
2021-12-03 22:28:19 +0900

    mecro optimize

--- auto.py
+++ auto.py
@@ -2,233 +2,81 @@ import keyboard
 import pyautogui
 from pynput.keyboard import Listener, Key, KeyCode
 
-def handlePress( key ):
+def pressKeys(*args):
+    for key in args:
+        pyautogui.press(key)
+
+def handlePress(key):
     #print( 'Press: {}'.format( key ) )
-
-
     if key == Key.esc:
         return False
-    #F
-    elif key == KeyCode(70):
+    #Reinforce
+    elif key == KeyCode(char='f'):
         pyautogui.keyDown('ctrlleft')
-        pyautogui.press('w')
-        pyautogui.press('s')
-        pyautogui.press('d')
-        pyautogui.press('a')
-        pyautogui.press('w')
+        pressKeys('w','s','d','a','w')
         pyautogui.keyUp('ctrlleft')
     #Railcannon Strike
-    #V
-    elif key == KeyCode(86):
+    elif key == KeyCode(char='v'):
         pyautogui.keyDown('ctrlleft')
-        pyautogui.press('d')
-        pyautogui.press('s')
-        pyautogui.press('w')
-        pyautogui.press('s')
-        pyautogui.press('a')
+        pressKeys('d','s','w','s','a')
         pyautogui.keyUp('ctrlleft')
     #Heavy Airstrike
-    #C
-    elif key == KeyCode(67):
+    elif key == KeyCode(char='c'):
         pyautogui.keyDown('ctrlleft')
-        pyautogui.press('d')
-        pyautogui.press('w')
-        pyautogui.press('s')
-        pyautogui.press('d')
-        pyautogui.press('a')
+        pressKeys('d','w','s','d','a')
         pyautogui.keyUp('ctrlleft')
     #Shredder Missile Strike
-    #X
-    elif key == KeyCode(88):
+    elif key == KeyCode(char='x'):
         pyautogui.keyDown('ctrlleft')
-        pyautogui.press('d')
-        pyautogui.press('a')
-        pyautogui.press('d')
-        pyautogui.press('a')
-        pyautogui.press('s')
-        pyautogui.press('s')
-        pyautogui.press('d')
+        pressKeys('d','a','d','a','s','s','d')
         pyautogui.keyUp('ctrlleft')
     #Thunderer Barrage
-    #Z
-    elif key == KeyCode(90):
+    elif key == KeyCode(char='z'):
         pyautogui.keyDown('ctrlleft')
-        pyautogui.press('d')
-        pyautogui.press('s')
-        pyautogui.press('w')
-        pyautogui.press('w')
-        pyautogui.press('a')
-        pyautogui.press('s')
+        pressKeys('d','s','w','w','a','s')
         pyautogui.keyUp('ctrlleft')
-
     #Resupply
     elif key == Key.f1:
         pyautogui.keyDown('ctrlleft')
-        pyautogui.press('s')
-        pyautogui.press('s')
-        pyautogui.press('w')
-        pyautogui.press('d')
+        pressKeys('s','s','w','d')
         pyautogui.keyUp('ctrlleft')
     #Runbler
     elif key == Key.f2:
         pyautogui.keyDown('ctrlleft')
-        pyautogui.press('s')
-        pyautogui.press('a')
-        pyautogui.press('w')
-        pyautogui.press('a')
-        pyautogui.press('a')
+        pressKeys('s','a','w','a','a')
         pyautogui.keyUp('ctrlleft')
     #Jump Pack
     elif key == Key.f3:
         pyautogui.keyDown('ctrlleft')
-        pyautogui.press('s')
-        pyautogui.press('w')
-        pyautogui.press('w')
-        pyautogui.press('s')
-        pyautogui.press('w')
+        pressKeys('s','w','w','s','w')
         pyautogui.keyUp('ctrlleft')
-    #drone
+    #Drone
     elif key == Key.f4:
         pyautogui.keyDown('ctrlleft')
-        pyautogui.press('a')
-        pyautogui.press('w')
-        pyautogui.press('d')
+        pressKeys('a','w','d')
         pyautogui.keyUp('ctrlleft')
     #Meltal Detector
     elif key == Key.f5:
         pyautogui.keyDown('ctrlleft')
-        pyautogui.press('s')
-        pyautogui.press('s')
-        pyautogui.press('d')
-        pyautogui.press('w')
+        pressKeys('s','s','d','w')
         pyautogui.keyUp('ctrlleft')
-            
         
-    
 def handleRelease(key):
     #print( 'Released: {}'.format( key ) )
-
     if key == Key.esc:
         return False
 
-with Listener(on_press=handlePress, on_release=handleRelease) as listener:
-    listener.join()
-
-'''
-while 1:
-    #Reinforce
-    if keyboard.read_key() == 'f':
-        if ~was_pressed:
-            was_pressed = True
-            pyautogui.keyDown('ctrlleft')
-            pyautogui.press('w')
-            pyautogui.press('s')
-            pyautogui.press('d')
-            pyautogui.press('a')
-            pyautogui.press('w')
-            pyautogui.keyUp('ctrlleft')
-    #Railcannon Strike
-    elif keyboard.read_key() == 'v':
-        if ~was_pressed:
-            was_pressed = True
-            pyautogui.keyDown('ctrlleft')
-            pyautogui.press('d')
-            pyautogui.press('s')
-            pyautogui.press('w')
-            pyautogui.press('s')
-            pyautogui.press('a')
-            pyautogui.keyUp('ctrlleft')
-    #Heavy Airstrike
-    elif keyboard.read_key() == 'c':
-        if ~was_pressed:
-            was_pressed = True
-            pyautogui.keyDown('ctrlleft')
-            pyautogui.press('d')
-            pyautogui.press('w')
-            pyautogui.press('s')
-            pyautogui.press('d')
-            pyautogui.press('a')
-            pyautogui.keyUp('ctrlleft')
-    #Shredder Missile Strike
-    elif keyboard.read_key() == 'x':
-        if ~was_pressed:
-            was_pressed = True
-            pyautogui.keyDown('ctrlleft')
-            pyautogui.press('d')
-            pyautogui.press('a')
-            pyautogui.press('d')
-            pyautogui.press('a')
-            pyautogui.press('s')
-            pyautogui.press('s')
-            pyautogui.press('d')
-            pyautogui.keyUp('ctrlleft')
-    #Thunderer Barrage
-    elif keyboard.read_key() == 'z':
-        if ~was_pressed:
-            was_pressed = True
-            pyautogui.keyDown('ctrlleft')
-            pyautogui.press('d')
-            pyautogui.press('s')
-            pyautogui.press('w')
-            pyautogui.press('w')
-            pyautogui.press('a')
-            pyautogui.press('s')
-            pyautogui.keyUp('ctrlleft')
-
-    #Resupply
-    elif keyboard.read_key() == 'f1':
-        if ~was_pressed:
-            was_pressed = True
-            pyautogui.keyDown('ctrlleft')
-            pyautogui.press('s')
-            pyautogui.press('s')
-            pyautogui.press('w')
-            pyautogui.press('d')
-            pyautogui.keyUp('ctrlleft')
-    #Runbler
-    elif keyboard.read_key() == 'f2':
-        if ~was_pressed:
-            was_pressed = True
-            pyautogui.keyDown('ctrlleft')
-            pyautogui.press('s')
-            pyautogui.press('a')
-            pyautogui.press('w')
-            pyautogui.press('a')
-            pyautogui.press('a')
-            pyautogui.keyUp('ctrlleft')
-    #Jump Pack
-    elif keyboard.read_key() == 'f3':
-        if ~was_pressed:
-            was_pressed = True
-            pyautogui.keyDown('ctrlleft')
-            pyautogui.press('s')
-            pyautogui.press('w')
-            pyautogui.press('w')
-            pyautogui.press('s')
-            pyautogui.press('w')
-            pyautogui.keyUp('ctrlleft')
-    #drone
-    elif keyboard.read_key() == 'f4':
-        if ~was_pressed:
-            was_pressed = True
-            pyautogui.keyDown('ctrlleft')
-            pyautogui.press('a')
-            pyautogui.press('w')
-            pyautogui.press('d')
-            pyautogui.keyUp('ctrlleft')
-    #Meltal Detector
-    elif keyboard.read_key() == 'f5':
-        if ~was_pressed:
-            was_pressed = True
-            pyautogui.keyDown('ctrlleft')
-            pyautogui.press('s')
-            pyautogui.press('s')
-            pyautogui.press('d')
-            pyautogui.press('w')
-            pyautogui.keyUp('ctrlleft')
-            
-    elif keyboard.read_key() == 'esc':
-            break
-    else:
-        was_pressed = False
-'''
+if __name__ == "__main__":
+    print('\nESC : quit mecro\n')
+    print('F : Reinforce')
+    print('V : Railcannon Strike')
+    print('C : Heavy Airstrike')
+    print('X : Shredder Missile Strike')
+    print('Z : Thunderer Barrage')
+    print('F1 : Resupply')
+    print('F2 : Runbler')
+    print('F3 : Jump Pack')
+    print('F4 : Drone')
+    print('F5 : Meltal Detector')
+    with Listener(on_press=handlePress, on_release=handleRelease) as listener:
+        listener.join()
